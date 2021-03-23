62ab71b495164aaec225635263a658d2dbe2ff39
Huygithub17 <vsnguyenduyhuy@gmail.com>
2021-03-23 14:43:56 +0700

    first commit

---
+++ bai1.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
+console.log('This example is different!');
+console.log('The result is displayed in the Command Line Interface');
+var http = require('http');
+var dt = require('./myfirstmodule');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
+}).listen(8080);
